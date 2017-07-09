a118c87c34943858f1837cd93424729cef64c468
Lawrence Ho <lawrenceqc@yahoo.com.hk>
2017-07-10 00:05:57 +0800

    fix LG bug

--- android/src/main/java/com/airbnb/android/react/maps/AirMapView.java
+++ android/src/main/java/com/airbnb/android/react/maps/AirMapView.java
@@ -83,38 +83,9 @@ public class AirMapView extends MapView implements GoogleMap.InfoWindowAdapter,
     private final ThemedReactContext context;
     private final EventDispatcher eventDispatcher;
 
-    private static boolean contextHasBug(Context context) {
-        return context == null ||
-            context.getResources() == null ||
-            context.getResources().getConfiguration() == null;
-    }
-
-    // We do this to fix this bug:
-    // https://github.com/airbnb/react-native-maps/issues/271
-    //
-    // which conflicts with another bug regarding the passed in context:
-    // https://github.com/airbnb/react-native-maps/issues/1147
-    //
-    // Doing this allows us to avoid both bugs.
-    private static Context getNonBuggyContext(ThemedReactContext reactContext) {
-        Context superContext = reactContext;
-
-        if (contextHasBug(superContext)) {
-            // we have the bug! let's try to find a better context to use
-            if (!contextHasBug(reactContext.getCurrentActivity())) {
-                superContext = reactContext.getCurrentActivity();
-            } else if (!contextHasBug(reactContext.getApplicationContext())) {
-                superContext = reactContext.getApplicationContext();
-            } else {
-                // ¯\_(ツ)_/¯
-            }
-        }
-        return superContext;
-    }
-
     public AirMapView(ThemedReactContext reactContext, AirMapManager manager,
             GoogleMapOptions googleMapOptions) {
-        super(getNonBuggyContext(reactContext), googleMapOptions);
+        super(reactContext.getCurrentActivity(), googleMapOptions);
 
         this.manager = manager;
         this.context = reactContext;
